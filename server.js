42ae474aacb070304114991179762f0541ff0496
ovevakev18-gif <ovevakev18@gmail.com>
2026-02-28 00:08:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
